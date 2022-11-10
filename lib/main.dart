b401958f90a992766b91e6d4fd68a0828498b17b
IFAMac <iliyasifa@gmail.com>
2022-11-10 22:22:35 +0530

    first commit command

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,55 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex++;
+    print(questionIndex);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'what\'s your car',
+    ];
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Quiz App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Hello'),
-          ),
-          body: const Text('Hello')),
+        appBar: AppBar(
+          title: const Text('My first App'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: const Text("Answer 1"),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: const Text("Answer 2"),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: const Text("Answer 3"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
