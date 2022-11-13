f75af456b08336e70a759d507e5560825feb445a
IFAMac <iliyasifa@gmail.com>
2022-11-13 11:55:36 +0530

    splitting the widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/answer.dart';
-import 'package:quiz_app/question.dart';
+import 'package:quiz_app/quiz.dart';
+import 'package:quiz_app/result.dart';
 
 void main() => runApp(const MyApp());
 
@@ -14,6 +14,74 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int _questionIndex = 0;
 
+  final List<Map<String, Object>> _questions = [
+    {
+      'question':
+          'Grand Central Terminal, Park Avenue, New York is the world\'s',
+      'answer': [
+        'largest railway station', 'highest railway station',
+        'longest railway station', 'None of the above',
+        // {'text': 'largest railway station', 'score': 1},
+        // {'text': 'highest railway station', 'score': 0},
+        // {'text': 'longest railway station', 'score': 0},
+        // {'text': 'None of the above', 'score': 0},
+      ],
+    },
+    {
+      'question': 'Entomology is the science that studies',
+      'answer': [
+        'The formation of rocks', 'Behavior of human beings', 'Insects',
+        'The origin and history of technical and scientific terms',
+        // {'text': 'Behavior of human beings', 'score': 0},
+        // {'text': 'Insects', 'score': 1},
+        // {
+        //   'text': 'The origin and history of technical and scientific terms',
+        //   'score': 0
+        // },
+        // {'text': 'The formation of rocks', 'score': 0},
+      ],
+    },
+    {
+      'question':
+          'Eritrea, which became the 182nd member of the UN in 1993, is in the continent of',
+      'answer': [
+        'Africa', 'Asia', 'Australia', 'Africa',
+        // {'text': 'Asia', 'score': 0},
+        // {'text': 'Europe', 'score': 0},
+        // {'text': 'Australia', 'score': 0},
+        // {'text': 'Africa', 'score': 1},
+      ],
+    },
+    // {
+    //   'question': 'Garampani sanctuary is located at',
+    //   'answer': [
+    //     {'text': 'Junagarh, Gujarat', 'score': 0},
+    //     {'text': 'Kohima, Nagaland', 'score': 0},
+    //     {'text': 'Diphu, Assam', 'score': 1},
+    //     {'text': 'Gangtok, Sikkim', 'score': 0},
+    //   ],
+    // },
+    // {
+    //   'question':
+    //       'For which of the following disciplines is Nobel Prize awarded?',
+    //   'answer': [
+    //     {'text': 'Physics and Chemistry', 'score': 0},
+    //     {'text': 'Physiology or Medicine', 'score': 0},
+    //     {'text': 'Literature, Peace and Economics', 'score': 0},
+    //     {'text': 'All of the above', 'score': 1},
+    //   ],
+    // },
+    // {
+    //   'question': 'Hitler party which came into power in 1933 is known as',
+    //   'answer': [
+    //     {'text': 'Nazi Party', 'score': 1},
+    //     {'text': 'Labour Party', 'score': 0},
+    //     {'text': 'Ku-Klux-Klan', 'score': 0},
+    //     {'text': 'Democratic Party', 'score': 0},
+    //   ],
+    // },
+  ];
+
   void _answerQuestion() {
     setState(() {
       _questionIndex++;
@@ -22,73 +90,6 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    List<Map<String, dynamic>> questions = [
-      {
-        'question':
-            'Grand Central Terminal, Park Avenue, New York is the world\'s',
-        'answer': [
-          'largest railway station', 'highest railway station',
-          'longest railway station', 'None of the above',
-          // {'text': 'largest railway station', 'score': 1},
-          // {'text': 'highest railway station', 'score': 0},
-          // {'text': 'longest railway station', 'score': 0},
-          // {'text': 'None of the above', 'score': 0},
-        ],
-      },
-      {
-        'question': 'Entomology is the science that studies',
-        'answer': [
-          'The formation of rocks', 'Behavior of human beings', 'Insects',
-          'The origin and history of technical and scientific terms',
-          // {'text': 'Behavior of human beings', 'score': 0},
-          // {'text': 'Insects', 'score': 1},
-          // {
-          //   'text': 'The origin and history of technical and scientific terms',
-          //   'score': 0
-          // },
-          // {'text': 'The formation of rocks', 'score': 0},
-        ],
-      },
-      {
-        'question':
-            'Eritrea, which became the 182nd member of the UN in 1993, is in the continent of',
-        'answer': [
-          'Africa', 'Asia', 'Australia', 'Africa',
-          // {'text': 'Asia', 'score': 0},
-          // {'text': 'Europe', 'score': 0},
-          // {'text': 'Australia', 'score': 0},
-          // {'text': 'Africa', 'score': 1},
-        ],
-      },
-      // {
-      //   'question': 'Garampani sanctuary is located at',
-      //   'answer': [
-      //     {'text': 'Junagarh, Gujarat', 'score': 0},
-      //     {'text': 'Kohima, Nagaland', 'score': 0},
-      //     {'text': 'Diphu, Assam', 'score': 1},
-      //     {'text': 'Gangtok, Sikkim', 'score': 0},
-      //   ],
-      // },
-      // {
-      //   'question':
-      //       'For which of the following disciplines is Nobel Prize awarded?',
-      //   'answer': [
-      //     {'text': 'Physics and Chemistry', 'score': 0},
-      //     {'text': 'Physiology or Medicine', 'score': 0},
-      //     {'text': 'Literature, Peace and Economics', 'score': 0},
-      //     {'text': 'All of the above', 'score': 1},
-      //   ],
-      // },
-      // {
-      //   'question': 'Hitler party which came into power in 1933 is known as',
-      //   'answer': [
-      //     {'text': 'Nazi Party', 'score': 1},
-      //     {'text': 'Labour Party', 'score': 0},
-      //     {'text': 'Ku-Klux-Klan', 'score': 0},
-      //     {'text': 'Democratic Party', 'score': 0},
-      //   ],
-      // },
-    ];
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Quiz App',
@@ -97,20 +98,15 @@ class _MyAppState extends State<MyApp> {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('My first App'),
+          title: const Text('Quiz'),
         ),
-        body: Column(
-          children: [
-            Question(
-                questionText: questions[_questionIndex]['question'].toString()),
-            ...questions[_questionIndex]['answer']
-                .map(
-                  (answer) => Answer(
-                      selectHandler: _answerQuestion, answerText: answer),
-                )
-                .toList(),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                questions: _questions,
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+              )
+            : const Result(),
       ),
     );
   }
---
+++ lib/quiz.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/answer.dart';
+import 'package:quiz_app/question.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final Function answerQuestion;
+  final int questionIndex;
+  const Quiz({
+    super.key,
+    required this.questions,
+    required this.answerQuestion,
+    required this.questionIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Question(
+          questionText: questions[questionIndex]['question'].toString(),
+        ),
+        ...(questions[questionIndex]['answer'] as List<String>)
+            .map(
+              (answer) => Answer(
+                selectHandler: answerQuestion,
+                answerText: answer,
+              ),
+            )
+            .toList(),
+      ],
+    );
+  }
+}
